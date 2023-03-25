f99675b340ae93b942b4308f858f937724c5307a
battlehawk <154075059@qq.com>
2023-03-26 06:24:21 +0800

    feat: fix bug
    Signed-off-by: battlehawk <154075059@qq.com>

--- src/utils/document.ts
+++ src/utils/document.ts
@@ -3,7 +3,7 @@ import { Selection, TextDocument } from "vscode";
 export const getSelectedLines = (selections: readonly Selection[], document: TextDocument) => {
   return selections.flatMap(selection => {
     const result: { number: number; text: string }[] = [];
-    for (let i = selection.start.line; i <= selection.end.line; i++) {
+    for (let i = selection.start.line + 1; i < selection.end.line; i++) {
       const text = document.lineAt(i).text;
       if (text.trim().length) result.push({ number: i, text });
     }
